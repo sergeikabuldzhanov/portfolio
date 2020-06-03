c26dacf9478d722b39b8e701af30abbee8aa8d7a
sergeikabuldzhanov <thesiegebot@gmail.com>
2020-06-03 12:49:53 +0500

    Checking if the problem is caused by sw plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -43,6 +43,6 @@ module.exports = {
         icon: `src/images/fev_icon.png`, // This path is relative to the root of the site.
       },
     }, // To learn more, visit: https://gatsby.dev/offline // this (optional) plugin enables Progressive Web App + Offline functionality
-    `gatsby-plugin-offline`,
+    // `gatsby-plugin-offline`,
   ],
 };
